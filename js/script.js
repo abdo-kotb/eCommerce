63dc5ab5a21bd3645acf5296b83e8af07305c273
abdo-kotb <86558336+abdo-kotb@users.noreply.github.com>
2022-01-05 00:37:45 +0400

    shop page commit

--- js/script.js
+++ js/script.js
@@ -1,33 +1,35 @@
 const slider = document.querySelector('.slider');
 
-let isDown = false,
-  startX,
-  scrollLeft;
+if (slider) {
+  let isDown = false,
+    startX,
+    scrollLeft;
 
-const stopSlide = function () {
-  isDown = false;
-  this.classList.remove('active');
-};
+  const stopSlide = function () {
+    isDown = false;
+    this.classList.remove('active');
+  };
 
-slider.addEventListener('mousedown', function (e) {
-  this.classList.add('active');
+  slider.addEventListener('mousedown', function (e) {
+    this.classList.add('active');
 
-  isDown = true;
-  startX = e.pageX - this.offsetLeft;
-  scrollLeft = slider.scrollLeft;
-});
+    isDown = true;
+    startX = e.pageX - this.offsetLeft;
+    scrollLeft = slider.scrollLeft;
+  });
 
-slider.addEventListener('mouseup', stopSlide);
-slider.addEventListener('mouseleave', stopSlide);
+  slider.addEventListener('mouseup', stopSlide);
+  slider.addEventListener('mouseleave', stopSlide);
 
-slider.addEventListener('mousemove', function (e) {
-  if (!isDown) return;
+  slider.addEventListener('mousemove', function (e) {
+    if (!isDown) return;
 
-  e.preventDefault();
-  const x = e.pageX - this.offsetLeft;
-  const move = x - startX;
-  this.scrollLeft = scrollLeft - move;
-});
+    e.preventDefault();
+    const x = e.pageX - this.offsetLeft;
+    const move = x - startX;
+    this.scrollLeft = scrollLeft - move;
+  });
+}
 
 const lazyImgs = document.querySelectorAll('[data-behavior="lazy"]');
 
@@ -49,3 +51,26 @@ const observeLazy = new IntersectionObserver(revealImgs, {
 });
 
 lazyImgs.forEach(img => observeLazy.observe(img));
+
+const rangeSlider = document.getElementById('slider');
+
+if (rangeSlider) {
+  noUiSlider.create(rangeSlider, {
+    start: [0, 450],
+    connect: true,
+    tooltips: true,
+    range: {
+      min: 0,
+      max: 500,
+    },
+  });
+}
+
+const marketImgs = document.querySelectorAll('.market__img');
+
+marketImgs.forEach(img =>
+  img.addEventListener('click', function () {
+    const marketCard = img.closest('.market__card');
+    marketCard.classList.toggle('bookmarked');
+  })
+);
